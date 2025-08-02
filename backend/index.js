b3b6f5b5ada645255861ee2ec0765027849a11d5
Francesco Peluso <francesco.peluso@agtweb.it>
2025-08-02 16:25:22 +0200

    add_cors backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const db = require('./database/conn');  
 const port = process.env.SERVER_PORT || 3000;
+const frontend = process.env.FRONT_URL || 3000;
+
+// Configurazione CORS
+// Questo permette a qualsiasi origine (*) di accedere alle tue API.
+// Per maggiore sicurezza in produzione, dovresti specificare solo le origini permesse.
+const corsOptions = {
+  origin: frontend, // Sostituisci con l'URL del tuo frontend Next.js in sviluppo
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Metodi HTTP permessi
+  credentials: true, // Permette l'invio di cookie/header di autorizzazione
+  optionsSuccessStatus: 204 // Alcuni browser richiedono 204 per le richieste OPTIONS
+};
+
+app.use(cors(corsOptions)); // Usa il middleware CORS con le opzion
 
 //CONFIGURAZIONE INIZIALE DELLE API
 app.use((req, res, next) => {
